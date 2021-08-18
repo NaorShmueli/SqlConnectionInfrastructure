e83bd988044befff166f817e6ff9afbc72727b13
naor2 <naor2@DESKTOP-2DV3RHL>
2021-08-18 15:33:22 +0300

    ef core structure

---
+++ SqlConnectionInfrastructure/EntityFrameworkExample/Startup.Logger.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Serilog;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EntityFrameworkExample
+{
+    public partial class Startup
+    {
+        private void ConfigureLogs(IServiceCollection services)
+        {
+            services.AddLogging(x => x.ClearProviders());
+            services.AddLogging(x => x.AddSerilog(new LoggerConfiguration().ReadFrom.Configuration(Configuration).Enrich.FromLogContext().CreateLogger()));
+
+        }
+    }
+}
---
+++ SqlConnectionInfrastructure/EntityFrameworkExample/Startup.Swagger.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.OpenApi.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+
+namespace EntityFrameworkExample
+{
+    public partial class Startup
+    {
+        private const string ApiName = "Entity Framework Example";
+
+        private void ConfigureSwagger(IServiceCollection services)
+        {
+            var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+            var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+            services.AddSwaggerGen(s =>
+            {
+                s.SwaggerDoc("v1", new OpenApiInfo { Title = ApiName, Version = "v1" });
+                s.IncludeXmlComments(xmlPath);
+            });
+        }
+
+        private void EnableSwagger(IApplicationBuilder app)
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI(s => { s.SwaggerEndpoint("/swagger/v1/swagger.json", ApiName); });
+        }
+    }
+}
